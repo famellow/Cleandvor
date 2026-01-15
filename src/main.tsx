cbde21649d2cc7e90823f186f4d73ce94752c57c
famellow <unfogetable16@gmail.com>
2026-01-15 17:45:40 +0700

    Настроил для выгрузки

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Обработка редиректа из 404.html для GitHub Pages
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect;
+  sessionStorage.removeItem("redirect");
+  const url = new URL(redirect);
+  if (url.searchParams.has("/")) {
+    const path = url.searchParams.get("/")?.replace(/~and~/g, "&");
+    if (path) {
+      window.history.replaceState(null, "", path + url.hash);
+    }
+  }
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
